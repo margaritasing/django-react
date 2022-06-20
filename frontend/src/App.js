330d37737c16cf6ea2e21097df96e7bad122394b
margaritasing <libiamfreitesm@gmail.com>
2022-06-20 17:44:31 -0300

    subo cambios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
-
+import { Provider } from 'react-redux';
+import store from './store'
 
 function App() {
   return (
+    <Provider store={store}>
+    <div className=''>
     <h1 className="text-3xl font-bold underline text-blue-600">
-    Hello world!
-  </h1>
+       Hello world!
+    </h1>
+   </div>
+   </Provider>
   );
 }
 
