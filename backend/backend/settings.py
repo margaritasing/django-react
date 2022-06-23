08264cf643d104db4ccf227fcd585cfb90b64197
margaritasing <libiamfreitesm@gmail.com>
2022-06-23 18:24:55 -0300

    subo actualizaciones

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -191,5 +191,13 @@ REST_FRAMEWORK = {
 }
 
 
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.google.GoogleOAuth2',
+    'social_core.backends.facebook.FacebookOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
